cfc58a096ed18a1708b5c1d287d7a3680b01bce2
GutoRech1990 <gutorech@me.com>
2023-09-06 11:17:56 +0200

    create angular-app

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurriculoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
